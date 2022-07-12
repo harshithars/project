eb0e460da6d23d86bd5afe8bdd91c16882a91bdd
R S <harshitha.r-s@capgemini.com>
2022-07-12 18:27:54 +0530

    database file

--- SweetCherry/src/main/java/com/sweetcherry/exception/CustomerNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.sweetcherry.exception;
-
-public class CustomerNotFoundException extends Exception {
-	public CustomerNotFoundException(String str) {
-		super(str);
-	}
-
-}
--- SweetCherry/src/main/java/com/sweetcherry/exception/NoSuchCustomerExists.java
+++
@@ -1,9 +0,0 @@
-package com.sweetcherry.exception;
-
-public class NoSuchCustomerExists extends Exception{
-	public NoSuchCustomerExists(String msg) {
-		super(msg);
-	}
-	
-
-}
